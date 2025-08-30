f150e3fea832262ea95ec79492bfd4b8063149c5
Ezequiel Brañanova <78105583+ezequielmartinb@users.noreply.github.com>
2025-08-30 12:20:13 -0300

    Delete pruebaApp directory

--- pruebaApp/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://qqpfmfkzckgpqkorexmv.supabase.co',
-  publicAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxcGZtZmt6Y2tncHFrb3JleG12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyMTQ3NjYsImV4cCI6MjA3MTc5MDc2Nn0.SOmv6tLPMogYoedP52ooD8JUjA6_v5NGIihTEpODrqg'
-};
--- pruebaApp/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { StatusBar } from '@capacitor/status-bar';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
-
-  StatusBar.hide();
