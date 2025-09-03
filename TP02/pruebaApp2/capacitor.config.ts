bb6a5f77205208a5a3a577be6b313fde828a13e9
ezequielmartinb10@gmail.com <ezequielmartinb10@gmail.com>
2025-09-02 21:00:29 -0300

    Cambio nombre de la app

--- TP02/pruebaApp2/capacitor.config.ts
+++ TP02/pruebaApp2/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.emb.app',
-  appName: 'Brañanova Ezequiel Martín',
+  appName: 'PruebaApp',
   webDir: 'www',
   plugins:
   {
